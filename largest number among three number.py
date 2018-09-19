759fdb874c8ff506748b85ae31342e8fa2f0487a
divyamuthaiah <41110108+divyamuthaiah@users.noreply.github.com>
2018-09-19 15:02:40 +0530

    Create largest number among three number.py

---
+++ largest number among three number.py
@@ -0,0 +1,8 @@
+num=float(input('enter the third number:'))
+if(num1>num2)and(num1>num3):
+   largest=num1
+elif(num2>num1)and(num2>num3):
+    largest=num2
+else:
+   largest=num3
+   print('the largest num is num3')
