4b5d49b0f7bd1de741bd6f838a098c256825449a
divyamuthaiah <41110108+divyamuthaiah@users.noreply.github.com>
2019-01-18 14:56:28 +0530

    Create display armstrong  twointravals.py

---
+++ display armstrong  twointravals.py
@@ -0,0 +1,14 @@
+
+lower = 1000
+upper = 2000
+
+lower = int(input("Enter lower range: "))
+upper = int(input("Enter upper range: "))
+sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit 
+       
+   if num == sum:
+       print(num)
