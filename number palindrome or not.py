71121c5107f6725da8372e268d001ab097bd0af1
divyamuthaiah <41110108+divyamuthaiah@users.noreply.github.com>
2019-01-18 13:34:20 +0530

    Create number palindrome or not.py

---
+++ number palindrome or not.py
@@ -0,0 +1,8 @@
+num = input('Enter any number : ')
+    val = int(num)
+    if num == str(num)
+        print('The given number is PALINDROME')
+    else:
+        print('The given number is NOT a palindrome')
+else:
+    print("Thats not a valid number")
